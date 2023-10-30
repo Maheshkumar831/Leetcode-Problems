69406cf039dda8429b2efecec46150554487698c
Mahesh Kumar R <114941303+Maheshkumar831@users.noreply.github.com>
2023-10-30 10:37:29 +0530

    Update AcronymOfWords.java

--- AcronymOfWords.java
+++ AcronymOfWords.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isAcronym(List<String> words, String s) {
         String str="";
         for(String w:words){
-            str+=(w.charAt(0));
+            str+=w.charAt(0);
             
         }
         if(str.equals(s)){
