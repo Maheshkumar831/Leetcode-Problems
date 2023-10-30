e76ccfe32a6ec50a3b6697cb6afd34f2341a6a31
Mahesh Kumar R <114941303+Maheshkumar831@users.noreply.github.com>
2023-10-30 10:51:29 +0530

    Update and rename Contains Duplicate to Contains Duplicate.java

---
+++ Contains Duplicate.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
+ /* int n=nums.length;                //Time Limit Exceeded
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]==nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;*/
